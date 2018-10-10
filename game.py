f7de2edb46d20708eedeec9b17ba9fbb0c25afce
88.pawel.kowalski@gmail.com <pavlozzz@live.com>
2018-10-10 20:49:03 +0200

    Cleanup in game.py

--- game.py
+++ game.py
@@ -15,11 +15,11 @@ class Game():
         self.players = []
         self.players_gen = self.players_generator()
         self.dealer = Dealer(self.table)
-        # self.carddeck = self.generate_deck()
         self.finished = False
         self.current_player = None
         self.round_no = 0
         self.game_results = []
+
 
     def player_action(self, params):
         if params['Action name'] == 'Bet':
@@ -38,95 +38,11 @@ class Game():
             self.current_player = self.get_next_player()
             return
 
+
     def initialize_game(self):
         self.dealer.deal_cards_to_players(self.players)
         self.check_game_state()
-
-    # def pick_a_card(self):
-    #     card = self.carddeck[random.randint(0,len(self.carddeck)-1)]
-    #     while card.taken == True:
-    #         card = self.carddeck[random.randint(0,len(self.carddeck)-1)]
-    #     card.taken = True
-    #     return card
-
-    def add_player(self, name):
-        self.players.append(Player(name))
-
-    def get_player(self, name):
-        return list(filter(lambda p: p.name == name, self.players))[0]
-
-    def print_table(self):
-        result='\n'
-        result+='Table\n'
-        for c in self.table:
-            result+=str(c)
-        result+='\n'
-        return result
-
-    # def generate_deck(self):
-    #     carddeck = []
-    #     figures = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
-    #     colors = ['♠', '♣', '♥', '♦']
-    #     carddeck = [Card(f, c) for f in figures for c in colors]
-    #     return carddeck
-
-    # def deal_cards_to_players(self):
-    #     for p in self.players:
-    #         p.cards = Hand(self.table)
-    #         p.add_card(self.pick_a_card())
-    #         p.add_card(self.pick_a_card())
-
-    def create_default_players(self, number_of_players):
-        players_tab = []
-        players_tab = [Player("Player " + str(i+1)) for i in range(number_of_players)]
-        return players_tab
-
-    # def add_new_card_to_table(self):
-    #     if self.round_no > 3:
-    #         self.finished = True
-    #         return
-    #     if self.round_no == 0:
-    #         return
-    #     if self.round_no == 1:
-    #         self.table.append(self.pick_a_card())
-    #         self.table.append(self.pick_a_card())
-    #         self.table.append(self.pick_a_card())
-    #     if self.round_no > 1:
-    #         self.table.append(self.pick_a_card())
-
-    def create_results_ranking(self, players_tab):
-        for p in players_tab:
-            p.find_hands()
-
-    def players_generator(self):
-        for p in self.players:
-            yield p
-
-    def get_current_player(self):
-        return self.current_player
-
-    def get_next_player(self):
-        if self.finished:
-            return None
-        try:
-            self.current_player = next(self.players_gen)
-            return self.current_player
-        except:
-            return None
-
-    def new_loop(self):
-        self.players_gen=self.players_generator()
-
-    def reset_round(self):
-        self.new_loop()
-        self.round_no += 1
-        if self.round_no > 3:
-            self.finished = True
-        self.dealer.add_new_card_to_table(self.round_no)
-        for p in self.players:
-            p.bet = 0
-            p.bet_placed = False
-
+        
     def check_betting_fished(self):
         max_bet = max(map(lambda p: p.bet, self.players))
         for p in self.players:
@@ -147,6 +63,50 @@ class Game():
                 "Hands": list(map(lambda h: h.as_name_and_value(),p.cards.hands_list)), 
                 "Cards": p.print_cards(),
                 "Best hand": p.cards.hands_list[0].as_name_and_value()})
-
         return self.game_results
-        #print(list(map(lambda p: p.name, players_tab)))
+
+    def create_results_ranking(self, players_tab):
+        for p in players_tab:
+            p.find_hands()
+
+
+    def add_player(self, name):
+        self.players.append(Player(name))
+
+    def get_player(self, name):
+        return list(filter(lambda p: p.name == name, self.players))[0]
+
+    def create_default_players(self, number_of_players):
+        players_tab = []
+        players_tab = [Player("Player " + str(i+1)) for i in range(number_of_players)]
+        return players_tab
+
+    def players_generator(self):
+        for p in self.players:
+            yield p
+
+    def get_current_player(self):
+        return self.current_player
+
+    def get_next_player(self):
+        if self.finished:
+            return None
+        try:
+            self.current_player = next(self.players_gen)
+            return self.current_player
+        except:
+            return None
+
+
+    def new_loop(self):
+        self.players_gen=self.players_generator()
+
+    def reset_round(self):
+        self.new_loop()
+        self.round_no += 1
+        if self.round_no > 3:
+            self.finished = True
+        self.dealer.add_new_card_to_table(self.round_no)
+        for p in self.players:
+            p.bet = 0
+            p.bet_placed = False
